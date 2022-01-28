6057ae2ed5ca0c712cad36421a2cb36436f2f9af
zenstacker <dlsghksll@naver.com>
2022-01-28 14:29:00 +0900

    Giving some variables to pug templet

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,28 +1,74 @@
+let fakeUser = {
+  username: "Nico",
+  loggedIn: true,
+};
+
+let boards = [
+  {
+    id: 1,
+    title: "First board",
+    comments: 7,
+    hitcounts: 14,
+  },
+  {
+    id: 2,
+    title: "Second board",
+    comments: 5,
+    hitcounts: 9,
+  },
+  {
+    id: 3,
+    title: "Third board",
+    comments: 1,
+    hitcounts: 5,
+  },
+];
+
+// localhost:4000
 export const trendingBoardForHome = (req, res) => {
   console.log("I'm trendingBoardForHome");
-  return res.render("home.pug");
+  return res.render("home.pug", {
+    pageTitle: "Home",
+    fakeUser: fakeUser,
+    boards: boards,
+  });
 };
 
+// localhost:4000/boards/:id
 export const watch = (req, res) => {
-  console.log(req.params);
-  return res.render("watch.pug", { params: req.params.id });
+  let board = boards[req.params.id - 1];
+
+  return res.render("watch.pug", {
+    pageTitle: `Watch ${board.title}`,
+    fakeUser: fakeUser,
+    board: board,
+  });
 };
 
+// localhost:4000/boards/:id/edit
 export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send(`<h1>Edit Board #${req.params.id}</h1>`);
+  let board = boards[req.params.id - 1];
+
+  return res.render("editBoard.pug", {
+    pageTitle: `Edit ${board.title}`,
+    fakeUser: fakeUser,
+    board: board,
+  });
 };
 
+// localhost:4000/boards/upload
 export const upload = (req, res) => {
   console.log("I'm handleUpload");
   return res.send("<h1>Upload Board</h1>");
 };
 
+// localhost:4000/search
 export const search = (req, res) => {
   console.log("I'm handleSearch");
   return res.send("<h1>Search Board</h1>");
 };
 
+// localhost:4000/boards/:id/delete
 export const deleteBoard = (req, res) => {
   console.log(req.params);
   return res.send(`<h1>Delete Board #${req.params.id}</h1>`);
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,23 +1,28 @@
+// localhost:4000/users/edit
 export const edit = (req, res) => {
   console.log("I'm handleEdit");
   return res.send("<h1>Edit User</h1>");
 };
 
+// localhost:4000/join
 export const join = (req, res) => {
   console.log("I'm handleJoin");
   return res.send("<h1>Join</h1>");
 };
 
+// localhost:4000/login
 export const login = (req, res) => {
   console.log("I'm handleLogin");
   return res.send("<h1>Login</h1>");
 };
 
+// localhost:4000/users/logout
 export const logout = (req, res) => {
   console.log("I'm handleLogout");
   return res.send("<h1>Logout</h1>");
 };
 
+// localhost:4000/users/:id
 export const profile = (req, res) => {
   console.log("I'm handleProfile");
   return res.send("<h1>My profile</h1>");
